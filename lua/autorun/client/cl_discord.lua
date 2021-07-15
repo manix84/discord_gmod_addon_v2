8c467a9ad7ec0cd0670d9b45ef5e2f0b40367f6a
Rob Taylor <manix84@gmail.com>
2021-07-15 22:52:36 +0100

    Adding support for adjustable image sizing, and deafen icon.

--- lua/autorun/client/cl_discord.lua
+++ lua/autorun/client/cl_discord.lua
@@ -2,17 +2,55 @@ AddCSLuaFile();
 
 CreateClientConVar("discord_show_mute_icon", true, true, false, "Sets the node bot endpoint.");
 
-resource.AddFile("materials/icon256/mute.png");
-muteIconAsset = Material("materials/icon256/mute.png", "smooth mips");
+local imgSize = 128
+local imgSpace = 32
+
+resource.AddFile("materials/icon" .. imgSize .. "/mute.png");
+resource.AddFile("materials/icon" .. imgSize .. "/deafen.png");
+local muteIconAsset = Material("materials/icon" .. imgSize .. "/mute.png", "smooth mips");
+local deafenIconAsset = Material("materials/icon" .. imgSize .. "/deafen.png", "smooth mips");
+
 
 shouldDrawMute = false;
 net.Receive("drawMute", function()
   shouldDrawMute = net.ReadBool();
 end);
 
-hook.Add("HUDPaint", "discord_HUDPaint", function()
-  if (not shouldDrawMute) then return; end
-  surface.SetDrawColor(176, 40, 40, 255);
-  surface.SetMaterial(muteIconAsset);
-  surface.DrawTexturedRect(32, 32, 256, 256);
+shouldDrawdeafen = false;
+net.Receive("drawdeafen", function()
+  shouldDrawdeafen = net.ReadBool();
+end);
+
+hook.Add("HUDPaint", "Discord_HUDPaint", function()
+  if (shouldDrawMute) then
+    surface.SetDrawColor(176, 40, 40, 255);
+    surface.SetMaterial(muteIconAsset);
+    surface.DrawTexturedRect(
+      imgSpace,
+      imgSpace,
+      (imgSize + imgSpace),
+      (imgSize + imgSpace)
+    );
+  end
+  if (shouldDrawDeafen) then
+    surface.SetDrawColor(176, 40, 40, 255);
+    surface.SetMaterial(deafenIconAsset);
+    if (shouldDrawMute) then
+      surface.DrawTexturedRect(
+        (imgSpace * 2) + imageSize,
+        (imgSpace * 2) + imageSize,
+        (imgSpace * 3) + (imageSize * 2),
+        (imgSpace * 3) + (imageSize * 2)
+      );
+    else
+      surface.DrawTexturedRect(
+        imgSpace, imgSpace,
+        (imgSize + imgSpace), (imgSize + imgSpace)
+      );
+    end
+  end
+end);
+
+hook.Add("HUDPaint", "Discord_HUDPaint", function()
+  if (not shouldDrawdeafen) then return; end
 end);

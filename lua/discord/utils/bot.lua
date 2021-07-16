a8af78841765fbf1e82f8774700100ac2316f007
Rob Taylor <manix84@gmail.com>
2021-07-16 19:53:29 +0100

    Creating a bot object to handle server requests and player actions.

--- lua/discord/utils/bot.lua
+++ lua/discord/utils/bot.lua
@@ -1,40 +1,69 @@
-bot = {};
+local post = "post";
+local get = "fetch";
 
-function bot:request(request, params, callback, tries)
+local function makeRequest(method, route, params, callback, tries)
   local defaultTries = 3;
+  if (not tries) then tries = defaultTries; end
+
   local endpoint  = GetConVar("discord_bot_endpoint"):GetString();
   local chatName  = GetConVar("discord_chat_name"):GetString();
   local authToken = GetConVar("discord_auth_token"):GetString();
+  local url = endpoint .. "/" .. route;
+  local authorization = "BEARER " .. authToken;
 
-  if (not tries) then tries = defaultTries; end
-
-  http.Fetch(endpoint .. "/" .. request, function(response)
-    local responseTable = util.JSONToTable(response)
-    if (responseTable.error and responseTable.error.msg) then
-      print("[" .. chatName .. "][Error] " .. resTable.error.msg);
-    end
-    callback(responseTable);
-  end, function(err)
+  local function onError(err)
     print("[" .. chatName .. "] Request to bot failed to respond. The Discord Bot may be offline, or the connection is blocked.");
     print("[" .. chatName .. "][Error] " .. err);
+  end
+  local function onSuccess(body, _size, _headers, code)
+    if (code ~= 200) then
+      onError(url .. " [" .. code .. "]");
+      return;
+    end;
+    print(url .. " [" .. code .. "]");
+    print(body);
 
+    local bodyTable = util.JSONToTable(body)
+    if (bodyTable.error and bodyTable.error.msg) then
+      print("[" .. chatName .. "][Error] " .. resTable.error.msg);
+    end
+    callback(bodyTable);
+  end
+  local function onFailure(err)
+    onError(err)
     if (tries ~= 0) then
       timer.Simple(((tries - default) + 1) * 0.5, function()
-        httpFetch(request, params, callback, tries - 1);
+        makeRequest(method, route, params, callback, tries - 1);
       end)
     end
-  end, {
-    ["authorization"] = "BEARER " .. authToken,
-    ["params"] = util.TableToJSON(params)
-  });
+  end
+
+  if (method == post) then
+    http.Post(url, params, onSuccess, onFailure, {
+      ["authorization"] = authorization
+    });
+  else
+    http.Fetch(url, onSuccess, onFailure, {
+      ["authorization"] = authorization,
+      ["params"] = util.TableToJSON(params)
+    });
+  end
 end
 
+bot = {};
+function bot:requestData(route, params, callback)
+  return makeRequest(
+    get, route, params, callback
+  );
+end
 function bot:playerAction(targetPly, action, params, callback)
+  if (targetPly:IsBot()) then return; end;
   local discordServerID = GetConVar("discord_server_id"):GetString();
   local steamUserID = targetPly:SteamID64();
-  return botRequest(
-    "servers/" .. discordServerID .. "/users/" .. steamUserID .. "/" .. action,
-    params,
-    callback
+  local route = "servers/" .. discordServerID ..
+                "/users/" .. steamUserID ..
+                "/" .. action;
+  return makeRequest(
+    post, route, params, callback
   );
 end

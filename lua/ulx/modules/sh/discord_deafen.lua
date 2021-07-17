331f524d0f7de44e0c0329350755a8ce7fafe80f
Rob Taylor <manix84@gmail.com>
2021-07-17 19:11:18 +0100

    Adding Mute and Deafen UXL Command modules.

---
+++ lua/ulx/modules/sh/discord_deafen.lua
@@ -0,0 +1,57 @@
+local CATEGORY_NAME = "Discord";
+
+function ulx.discordDeafen(callingPly, targetPlys, duration, shouldUndeafen)
+  if (shouldUndeafen) then
+    for i = 1, #targetPlys do
+      hook.Run("UndeafenPlayer",
+        targetPlys[i],
+        callingPly:Name() .. " requested from ULX menu"
+      );
+    end
+
+    ulx.fancyLogAdmin(
+      callingPly, "#A un-deafend #T", targetPlys
+    );
+  else
+    for i = 1, #targetPlys do
+      hook.Run("DeafenPlayer",
+        targetPlys[i],
+        callingPly:Name() .. " requested from ULX menu",
+        duration
+      );
+    end
+
+    ulx.fancyLogAdmin(
+      callingPly, "#A deafend #T for #i seconds", targetPlys, duration
+    );
+  end
+end
+
+local discordDeafen = ulx.command(
+  CATEGORY_NAME,
+  "ulx deafen",
+  ulx.discordDeafen,
+  "!deafen"
+);
+
+discordDeafen:addParam{
+  type = ULib.cmds.PlayersArg
+};
+discordDeafen:addParam{
+  type = ULib.cmds.NumArg,
+  min = 0,
+  max = 720,
+  default = 5,
+  hint = "duration, 0 is until round end",
+  ULib.cmds.optional,
+  ULib.cmds.round
+};
+discordDeafen:addParam{
+  type = ULib.cmds.BoolArg,
+  invisible = true
+};
+
+discordDeafen:setOpposite("ulx undeafen", {_, _, _, true}, "!undeafen");
+
+discordDeafen:defaultAccess(ULib.ACCESS_OPERATOR);
+discordDeafen:help("Deafen and un-deafen the player in Discord");
---
+++ lua/ulx/modules/sh/discord_mute.lua
@@ -0,0 +1,57 @@
+local CATEGORY_NAME = "Discord";
+
+function ulx.discordMute(callingPly, targetPlys, duration, shouldUnmute)
+  if (shouldUnmute) then
+    for i = 1, #targetPlys do
+      hook.Run("UnmutePlayer",
+        targetPlys[i],
+        callingPly:Name() .. " requested from ULX menu"
+      );
+    end
+
+    ulx.fancyLogAdmin(
+      callingPly, "#A un-muted #T", targetPlys
+    );
+  else
+    for i = 1, #targetPlys do
+      hook.Run("MutePlayer",
+        targetPlys[i],
+        callingPly:Name() .. " requested from ULX menu",
+        duration
+      );
+    end
+
+    ulx.fancyLogAdmin(
+      callingPly, "#A muted #T for #i seconds", targetPlys, duration
+    );
+  end
+end
+
+local discordMute = ulx.command(
+  CATEGORY_NAME,
+  "ulx mute",
+  ulx.discordMute,
+  "!mute"
+);
+
+discordMute:addParam{
+  type = ULib.cmds.PlayersArg
+};
+discordMute:addParam{
+  type = ULib.cmds.NumArg,
+  min = 0,
+  max = 720,
+  default = 5,
+  hint = "duration, 0 is until round end",
+  ULib.cmds.optional,
+  ULib.cmds.round
+};
+discordMute:addParam{
+  type = ULib.cmds.BoolArg,
+  invisible = true
+};
+
+discordMute:setOpposite("ulx unmute", {_, _, _, true}, "!unmute");
+
+discordMute:defaultAccess(ULib.ACCESS_OPERATOR);
+discordMute:help("Mute and un-mute the player in Discord");
